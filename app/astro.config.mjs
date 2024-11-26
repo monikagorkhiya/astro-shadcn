990c31327ed70b82c116d771ee3b0c91e0143806
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-11-26 17:27:46 +0530

    fix: config file

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://monikagorkhiya.github.io/astro-shadcn/",
   site: 'https://astronaut.github.io',
   base: 'astro-shadcn',
   integrations: [

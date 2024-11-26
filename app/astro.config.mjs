a36160635db7693256aacd91c9aaa4c799b56bf5
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-11-26 16:50:27 +0530

    fix: build error

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -7,11 +7,12 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.improwised.com/",
+  site: "https://monikagorkhiya.github.io/astro-shadcn/",
   integrations: [
     tailwind(),
     icon(),
     sitemap(),
+
     partytown({
       config: {
         forward: ["dataLayer.push"],
